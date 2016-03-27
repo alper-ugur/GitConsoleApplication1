f1794e40f0a40ff25b498dc9be3e6d16dd55b9bf
alperugurdev <alper_ugur@yahoo.com>
2016-03-27 19:30:15 +0300

    ek

--- GitConsoleApplication1/Program.cs
+++ GitConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace GitConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
+            Console.Write("again");
         }
     }
 }
